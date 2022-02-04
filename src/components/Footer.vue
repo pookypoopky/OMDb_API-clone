eaf8d8b41346894ca658d2e85089a469ce9e1517
pookypoopky <pooky1997@naver.com>
2022-02-04 16:56:29 +0900

    Create project

---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer>
+    <Logo />
+    <a
+      href="https://github.com/pookypoopky"
+      target="_blank">
+      (c){{ new Date().getFullYear() }} pookypoopky
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  padding: 70px 0;
+  text-align: center;
+  opacity: .3;
+  a {
+    text-decoration: none;
+  }
+  .logo {
+    display: block;
+    margin-bottom: 4px;
+  }
+}
+</style>
